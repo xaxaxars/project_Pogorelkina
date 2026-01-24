4387fc5cd1263830d6de187eaa7652b656fa747b
xaxaxars <angelinasvet7@gmail.com>
2026-01-24 12:47:19 +0300

    Update PZ_5.1.py

--- PZ_5/PZ_5.1.py
+++ PZ_5/PZ_5.1.py
@@ -1,24 +1,9 @@
-
-# Функция, которая выводит на экран строку из заданного количества символов
-
-def print_string(length):
-    s = input("Введите символы для вывода: ")
-    # Если введённая строка длиннее нужной длины, обрежем её
-    if len(s) > length:
-        s = s[:length]
-    # Если короче, дополним пробелами
-    elif len(s) < length:
-        s = s + " " * (length - len(s))
-    print(s)
-
-while True:
-    try:
-        n = int(input("Введите количество символов в строке: "))
-        if n > 0:
-            break
-        else:
-            print("Введите число больше нуля.")
-    except ValueError:
-        print("Неправильно ввели! Попробуйте ещё раз.")
-
-print_string(n)
+#Найти сумму чисел ряда 1,2,3,...,60 с использованием функции нахождения суммы.
+#Использовать локальные переменные.
+def sum_of_series(n):
+    total = 0
+    for i in range(1, n + 1):
+        total += i
+    return total
+result = sum_of_series(60)
+print(f"Сумма чисел от 1 до 60: {result}")
