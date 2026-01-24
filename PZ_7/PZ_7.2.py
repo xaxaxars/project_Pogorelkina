e7e093e4fa7fd9dbda11904f53ab3052b5e2edc2
xaxaxars <angelinasvet7@gmail.com>
2026-01-24 12:49:30 +0300

    Update PZ_7.2.py

--- PZ_7/PZ_7.2.py
+++ PZ_7/PZ_7.2.py
@@ -1,27 +1,9 @@
-# Функция вычисляет, на сколько квадратов разрезать прямоугольник A x B,
-# отрезая каждый раз квадрат наибольшей стороны.
-
-def count_squares(A, B):
-    count = 0
-    while A != 0 and B != 0:
-        if A > B:
-            count += A // B
-            A = A % B
-        else:
-            count += B // A
-            B = B % A
-    return count
-
-while True:
-    try:
-        A = int(input("Введите длину стороны A (натуральное число): "))
-        B = int(input("Введите длину стороны B (натуральное число): "))
-        if A > 0 and B > 0:
-            break
-        else:
-            print("Введите натуральные числа больше нуля.")
-    except ValueError:
-        print("Неправильно ввели! Попробуйте ещё раз.")
-
-result = count_squares(A, B)
-print("Можно разрезать на", result, "квадратов")
+#Дана строка, состоящая из русских слов, разделенных пробелами (одним или
+#несколькими). Найти длину самого длинного слова
+text = input("Введите строку: ")
+words = text.split()
+if not words: 
+    print("Строка не содержит слов!")
+else:
+    max_length = max(len(word) for word in words)
+    print(f"Длина самого длинного слова: {max_length}")
